a8b5c579d9c0e4dff069478ee71a27dfd76333ff
= <122753731+JadaWoods626@users.noreply.github.com>
2023-06-09 16:06:45 -0500

    Portfolio Update

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+// these functions open and close the contact form
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
